5c6933fa5b4f3a40eac66238fefc3cafb6c004f5
CDoohan <pereiradoohan@gmail.com>
2019-07-19 11:32:25 -0300

    Adicionando mais models e Cookies

--- NodeJS/conFusionServer/routes/leaderRouter.js
+++ NodeJS/conFusionServer/routes/leaderRouter.js
@@ -2,57 +2,80 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const leaderRouter = express.Router();
 
+const Leaders = require('../models/leaders');
+
 /**
  * initialize the body parser to be used.
  */
 leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    next()
-})
 .get((req, res) => {
-    res.end('Will send all the leaders to you!')
+    Leaders.find({})
+        .then((leaders) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(leaders);
+        }, (err) => next(err))
+    .catch((err) => next(err));
 })
 .post((req, res, next) => {
-    if (typeof req.body.name !== "undefined" && typeof req.body.description !== "undefined") {
-        res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
-    }
-    else {
-        res.end("POST method for leaders require name & description, please submit the appropirate data. (Also verify MIME TYPE is application/json)");
-    }
+    Leaders.create(req.body)
+        .then((leader) => {
+            console.log('Leader Created', leader);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(leader);
+        }, err => next(err))
+    .catch( (err) => next(err))
 })
 .put((req,res,next) => {
     res.statusCode = 403//403 significa que não é suportado
     res.end('PUT operation not supported on Leaders')
 })
 .delete((req, res) => {
-    res.end('Deleting all the leaders!')
+    Leaders.remove({})
+        .then((response) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(response);
+        }, err => next(err))
+    .catch( err => next(err))
 })
 
 leaderRouter.route('/:leaderId')
 .get((req, res) => {
-    res.end('Will send the details of the leader: '+ req.params.leaderId)
+    Leaders.findById(req.params.leaderId)
+    .then((leader) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(leader);
+    }, err => next(err))
+    .catch( err => next(err))
 })
 .post((req,res,next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /leaders/'+ req.params.leaderId);
 })
 .put((req, res, next) => {
-    if (typeof req.body.name !== "undefined" && typeof req.body.description !== "undefined"){
-        res.write("Updating the leader: " + req.params.leaderId );
-        res.end(' Will update the leader: ' + req.body.name + ' with details: ' + req.body.description);
-    } else {
-        res.end("POST method for leaders/"+ req.params.leaderId +
-            " requires name & description, please submit the appropirate data."+
-            "Also verify MIME TYPE is application/json"
-        );
-    }
+    Leaders.findByIdAndUpdate(req.params.leaderId, {
+        $set: req.body
+    }, { new : true })
+        .then( leader => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(leader);
+        }, err => next(err))
+    .catch( err => next(err))
 })
 .delete((req, res) => {
-    res.end('Deleting leader: ' + req.params.leaderId);
+    Leaders.findByIdAndRemove(req.params.leaderId)
+        .then( leader => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(leader);
+        }, err => next(err))
+    .catch( err => next(err))
 })
 
 module.exports = leaderRouter;
--- NodeJS/conFusionServer/routes/promoRouter.js
+++ NodeJS/conFusionServer/routes/promoRouter.js
@@ -2,58 +2,80 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const promoRouter = express.Router();
 
+const Promotions = require('../models/promotions');
+
 /**
  * initialize the body parser to be used.
  */
 promoRouter.use(bodyParser.json());
 
 promoRouter.route('/')
-.all((req,res,next) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    next()
-})
 .get((req, res) => {
-    res.end('Will send all the promotions to you!')
+    Promotions.find({})
+        .then((promos) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promos);
+        }, (err) => next(err))
+        .catch((err) => next(err));
 })
 .post((req,res,next) => {
-    if (typeof req.body.name !== "undefined" && typeof req.body.description !== "undefined") {
-        res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
-    }
-    else {
-        res.end("POST method for promotions require name & description, please submit the appropirate data. (Also verify MIME TYPE is application/json)");
-    }
+    Promotions.create(req.body)
+        .then((promo) => {
+            console.log('Promotion Created', promo);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promo);
+        }, err => next(err))
+        .catch( (err) => next(err))
 })
 .put((req,res,next) => {
     res.statusCode = 403//403 significa que não é suportado
     res.end('PUT operation not supported on Promotions')
 })
 .delete((req, res) => {
-    res.end('Deleting all the promotions!')
+    Promotions.remove({})
+        .then((response) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(response);
+        }, err => next(err))
+        .catch( err => next(err))
 })
 
 promoRouter.route('/:promoId')
 .get((req, res) => {
-    res.end('Will send the details of the promotion: '+ req.params.promoId)
+    Promotions.findById(req.params.promoId)
+    .then((promo) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(promo);
+    }, err => next(err))
+    .catch( err => next(err))
 })
 .post((req,res,next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /promotions/'+ req.params.promoId);
 })
 .put((req,res,next) => {
-    if (typeof req.body.name !== "undefined" && typeof req.body.description !== "undefined") {
-        res.write("Updating the promotion: " + req.params.promoId );
-        res.end(' Will update the promotion: ' + req.body.name + ' with details: ' + req.body.description);
-    } 
-    else {
-        res.end("POST method for promotions/"+ req.params.promoId +
-            " requires name & description, please submit the appropirate data."+
-            "Also verify MIME TYPE is application/json"
-        );
-    }
+    Promotions.findByIdAndUpdate(req.params.promoId, {
+        $set: req.body
+    }, { new : true })
+        .then( promo => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promo);
+        }, err => next(err))
+        .catch( err => next(err))
 })
 .delete((req, res) => {
-    res.end('Deleting promotion: ' + req.params.promoId);
+    Promotions.findByIdAndRemove(req.params.promoId)
+        .then( promo => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promo);
+        }, err => next(err))
+        .catch( err => next(err))
 })
 
 module.exports = promoRouter;
