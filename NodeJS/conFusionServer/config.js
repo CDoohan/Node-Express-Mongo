b73d78f13197ae4a8bf4d69bf86b9f8b9d50972b
CDoohan <pereiradoohan@gmail.com>
2019-07-30 16:00:36 -0300

    Passport Facebook

--- NodeJS/conFusionServer/config.js
+++ NodeJS/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '350394409207082',
+        clientSecret: '8ac17459683e4c21b7eb74fbe94a6810'
+    }
 }
